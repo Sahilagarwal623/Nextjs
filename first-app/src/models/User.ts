926e38ac12046500692a84ea3d306e2ab89b2f61
Sahilagarwal623 <agarwalsahil333@gmail.com>
2025-08-07 14:58:36 +0530

    initial commit

---
+++ first-app/src/models/User.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    messages: Message[];
+    createdAt: Date;
+    verifyCode: string;
+    verifyCodeExpires: Date;
+    isAcceptingMessages: boolean;
+}
+const UserSchema: Schema<User> = new Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    messages: [MessageSchema],
+    createdAt: { type: Date, default: Date.now },
+    verifyCode: { type: String, required: true },
+    verifyCodeExpires: { type: Date, required: true },
+    isAcceptingMessages: { type: Boolean, default: true }
+});
+
