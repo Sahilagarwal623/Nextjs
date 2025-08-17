264299a304b09dfaa6e212ed166114d6c29d21d5
Sahilagarwal623 <agarwalsahil333@gmail.com>
2025-08-17 18:55:21 +0530

    new commit

---
+++ first-app/src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold mb-4">Sign In</h1>
+      <p className="text-gray-600">This is the sign-in page.</p>
+      <input className="text-white"></input>
+    </div>
+  );
+}
